32aa2a47eeb9b27e7cba4edde12a38a95dbf0e60
Mikita Brodau <Mikita_Brodau@epam.com>
2021-01-04 14:55:37 +0300

    add say Hello, (name) feature

---
+++ hello/src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static String sayHello(String s){
+        return "Hello, " + s;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(HelloWorld.sayHello("name"));
+    }
+}
